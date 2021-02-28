fb9854a37a3fa13888794c3870b141a8cf4680f1
Gyan <gyanadesh1998@gmail.com>
2021-02-28 20:14:01 +0530

    modified url2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App(){
   const [city,setCity]=React.useState("");
   const [cityWeather,setCityWeather]=React.useState({});
   const fetchCityWeather=()=>{
-    fetch(`https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=67ec045673b3933175daaa40e2f8a618`)
+    fetch(`https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=67ec045673b3933175daaa40e2f8a618`)
     .then((response)=>response.json())
     .then((result)=>{
       setCityWeather(result);
